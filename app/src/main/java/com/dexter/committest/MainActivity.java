16d34176ed45c0b516cfd622b2f8219eef26f555
Lucas Moura <63266283+rivotrilads@users.noreply.github.com>
2020-04-07 00:26:15 -0300

    Preparação do ambiente para introdução do método do clique do botão

--- app/src/main/java/com/dexter/committest/MainActivity.java
+++ app/src/main/java/com/dexter/committest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Preparação para introdução do método no clique do botão
     }
 }
