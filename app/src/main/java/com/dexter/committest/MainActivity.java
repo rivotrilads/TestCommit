c820f0fb00e4400206e1571b523f946fb36fda71
Tiago <cleitonneri04@gmail.com>
2020-04-07 20:53:38 -0300

    Mensagem de Toast implementada

--- app/src/main/java/com/dexter/committest/MainActivity.java
+++ app/src/main/java/com/dexter/committest/MainActivity.java
@@ -2,14 +2,32 @@ package com.dexter.committest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView t;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Preparação para introdução do método no clique do botão
+
+        b = findViewById(R.id.botao);
+        t = findViewById(R.id.label);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Testando Mensagem de Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
